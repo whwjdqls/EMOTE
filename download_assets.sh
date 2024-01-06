56c251ef856efd1b72f2bd318b59bfaf0f992fda
jiiiisoo <jisoo6687@yonsei.ac.kr>
2024-01-06 17:10:14 +0900

    changed assets for lip reading model

--- download_assets.sh
+++ download_assets.sh
@@ -29,11 +29,13 @@ wget https://download.is.tue.mpg.de/emote/FLAME.zip -O FLAME.zip --no-check-cert
 echo "Extracting FLAME..."
 ## unzip without overwriting existing files
 unzip -n FLAME.zip
+rm FLAME.zip
 
 echo "Downloading FLINT"
 wget https://download.is.tue.mpg.de/emote/MotionPrior.zip  --no-check-certificate
 echo "Extracting FLINT..."
 unzip -n MotionPrior.zip
+rm MotionPrior.zip
 cd ..
 
 echo "Downloading static emotion feature extractor" 
@@ -43,16 +45,29 @@ cd models/EmotionRecognition/image_based_networks
 wget https://download.is.tue.mpg.de/emoca/assets/EmotionRecognition/image_based_networks/ResNet50.zip -O ResNet50.zip --no-check-certificate
 echo "Extracting ResNet 50"
 unzip -n ResNet50.zip
+rm ResNet50.zip
 cd ../..
 
 echo "Downloading Video Emotion Recognition net"
 wget https://download.is.tue.mpg.de/emote/VideoEmotionRecognition.zip --no-check-certificate
 echo "Extracting Video Emotion Recognition net"
 unzip -n VideoEmotionRecognition.zip
+rm VideoEmotionRecognition.zip
 
 echo "Downloading EMOTE..."
 wget https://download.is.tue.mpg.de/emote/TalkingHead.zip --no-check-certificate
 echo "Extracting EMOTE..."
 unzip -n TalkingHead.zip
+rm TalkingHead.zip
+
+cd ../externals/spectre
+
+echo -e "\nDownloading lipreading pretrained model..."
+
+FILEID=1yHd4QwC7K_9Ro2OM_hC7pKUT2URPvm_f
+FILENAME=LRS3_V_WER32.3.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILEID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
+unzip $FILENAME -d data/
+rm LRS3_V_WER32.3.zip
 
 echo "Assets for EMOTE downloaded and extracted."
